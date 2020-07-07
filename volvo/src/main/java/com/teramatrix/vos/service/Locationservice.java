5c8ef76d0291cc5771bdbba01759e7467aba0aa5
Vaibhav <vaibhav.jain@systematixindia.com>
2020-07-07 12:48:04 +0530

    Update Location firebase channel Id

--- volvo/src/main/java/com/teramatrix/vos/service/Locationservice.java
+++ volvo/src/main/java/com/teramatrix/vos/service/Locationservice.java
@@ -1,14 +1,18 @@
 package com.teramatrix.vos.service;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
@@ -29,217 +33,246 @@ import java.util.TimerTask;
 
 public class Locationservice extends Service implements
         OnConnectionFailedListener, ConnectionCallbacks, LocationListener {
-    public static final String TAG = "Locationservice";
-    public static GoogleApiClient mGoogleApiClient;
-    public static final long UPDATE_LOCATION_INTERVAL = 60 * 1000;
-    //    PubnubHandler pubnubHandler;
-    private LocationRequest mLocationRequest;
-    private boolean mRequestingLocationUpdates = false;
-    //Declare the timer
-    Timer locationPostTimer;
-    static Locationservice instans;
+  public static final String TAG = "Locationservice";
+  public static GoogleApiClient mGoogleApiClient;
+  public static final long UPDATE_LOCATION_INTERVAL = 60 * 1000;
+  //    PubnubHandler pubnubHandler;
+  private LocationRequest mLocationRequest;
+  private boolean mRequestingLocationUpdates = false;
+  //Declare the timer
+  Timer locationPostTimer;
+  static Locationservice instans;
 
-    public static Locationservice getInstans() {
-        return instans;
+  public static Locationservice getInstans() {
+    return instans;
+  }
+
+  public static Intent createIntent(Context context) {
+    return new Intent(context, Locationservice.class);
+  }
+
+  private void printLog(String message) {
+    Log.e(TAG, message);
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    instans = this;
+
+    // TODO Auto-generated method stub
+    //  Utils.printLog("my service onStartCommand");
+    printLog("onCreate call");
+    buildGoogleApiClient();
+    mRequestingLocationUpdates = false;
+    triggerLocationPostTimer();
+  }
+
+  @Override
+  public int onStartCommand(Intent intent, int flags, int startId) {
+    printLog("onStartCommand call");
+    buildGoogleApiClient();
+    mRequestingLocationUpdates = false;
+    triggerLocationPostTimer();
+    super.onStartCommand(intent, flags, startId);
+    return START_STICKY;
+
+  }
+
+  @Override
+  public IBinder onBind(Intent intent) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public void onConnected(Bundle arg0) {
+    // TODO Auto-generated method stub
+    //  Utils.printLog("my service GoogleApiclient onConnected");
+    printLog("GoogleApiclient onConnected call");
+    createLocationRequest();
+    if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+      return;
     }
-
-    public static Intent createIntent(Context context) {
-        return new Intent(context, Locationservice.class);
+    Location mLastLocation = LocationServices.FusedLocationApi
+            .getLastLocation(mGoogleApiClient);
+    UtilityFunction.saveLocationToPreference(getApplicationContext(), mLastLocation);
+    startLocationUpdates();
+    //Notification
+    //Notification
+    String chanelId = "";
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+      chanelId = createNotificationChannel("vas_Service", "vas background service");
     }
+    startForeground(1119,
+            crteateForgroundNotification(getApplicationContext(), "Tracking Service", chanelId));
+  }
 
-    private void printLog(String message) {
-        Log.e(TAG, message);
+  @RequiresApi(Build.VERSION_CODES.O)
+  private String createNotificationChannel(String channelId, String channelName) {
+
+    NotificationChannel notificationChannel = new NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH);
+    notificationChannel.enableLights(true);
+    notificationChannel.enableVibration(true);
+    notificationChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+    NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+    notificationManager.createNotificationChannel(notificationChannel);
+    return channelId;
+
+  }
+
+  @Override
+  public void onConnectionSuspended(int arg0) {
+    // TODO Auto-generated method stub
+    printLog("GoogleApiclient onConnectionSuspended call");
+    mGoogleApiClient.connect();
+    //   Utils.printLog("my service GoogleApiclient onConnectionSuspended");
+
+  }
+
+  @Override
+  public void onConnectionFailed(ConnectionResult arg0) {
+    printLog("onConnectionFailed call");
+    printLog(arg0.toString());
+    // TODO Auto-generated method stub
+    //  Utils.printLog("my service GoogleApiclient onConnectionFailed");
+  }
+
+  @Override
+  public void onLocationChanged(Location updatelocation) {
+    printLog("onLocationChanged call");
+
+    // TODO Auto-generated method stub
+    UtilityFunction.saveLocationToPreference(getApplicationContext(), updatelocation);
+
+    if (updatelocation != null)
+      printLog("location lat:" + updatelocation.getLatitude() + " , lng:" + updatelocation.getLongitude());
+  }
+
+  protected synchronized void buildGoogleApiClient() {
+    if (mGoogleApiClient != null && mGoogleApiClient.isConnected()) {
+      stopLocationUpdates();
+      mGoogleApiClient.disconnect();
     }
+    mGoogleApiClient = new GoogleApiClient.Builder(this)
+            .addConnectionCallbacks(this)
+            .addOnConnectionFailedListener(this)
+            .addApi(LocationServices.API)
+            .build();
+    mGoogleApiClient.connect();
+    printLog("buildGoogleApiClient call");
+  }
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        instans = this;
+  protected void createLocationRequest() {
+    printLog("createLocationRequest call");
+    mLocationRequest = LocationRequest.create();
+    mLocationRequest.setInterval(UPDATE_LOCATION_INTERVAL);
+    mLocationRequest.setFastestInterval(UPDATE_LOCATION_INTERVAL);
+    //  mLocationRequest.setSmallestDisplacement(5);
+    mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+  }
 
-        // TODO Auto-generated method stub
-        //  Utils.printLog("my service onStartCommand");
-        printLog("onCreate call");
-        buildGoogleApiClient();
-        mRequestingLocationUpdates = false;
-        triggerLocationPostTimer();
+  protected void startLocationUpdates() {
+    printLog("startLocationUpdates call");
+
+    if (!mRequestingLocationUpdates && mGoogleApiClient != null && mGoogleApiClient.isConnected()) {
+      mRequestingLocationUpdates = true;
+      if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        return;
+      }
+      LocationServices.FusedLocationApi.requestLocationUpdates(
+              mGoogleApiClient, mLocationRequest, this);
     }
+  }
 
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        printLog("onStartCommand call");
-        buildGoogleApiClient();
-        mRequestingLocationUpdates = false;
-        triggerLocationPostTimer();
-        super.onStartCommand(intent, flags, startId);
-        return START_STICKY;
+  protected void stopLocationUpdates() {
+    printLog("stopLocationUpdates call");
+
+    if (mGoogleApiClient != null && mGoogleApiClient.isConnected()
+            && mRequestingLocationUpdates) {
+      mRequestingLocationUpdates = false;
+      LocationServices.FusedLocationApi.removeLocationUpdates(
+              mGoogleApiClient, this);
+    }
+    if (locationPostTimer != null) {
+      locationPostTimer.cancel();
+      locationPostTimer = null;
+    }
+  }
+
+
+  @Override
+  public void onDestroy() {
+    // TODO Auto-generated method stub
+    instans = null;
+
+    stopLocationUpdates();
+    //  Utils.printLog("Service destroy");
+    super.onDestroy();
+  }
+
+  private void triggerLocationPostTimer() {
+    printLog("triggerLocationPostTimer");
+    if (locationPostTimer != null) {
+      locationPostTimer.cancel();
 
     }
+    locationPostTimer = new Timer();
+    //Set the schedule function and rate
+    locationPostTimer.scheduleAtFixedRate(new TimerTask() {
 
-    @Override
-    public IBinder onBind(Intent intent) {
-        // TODO Auto-generated method stub
-        return null;
+                                            @Override
+                                            public void run() {
+                                              //Called each time when 1000 milliseconds (1 second) (the period parameter)
+                                              //Trigger anther servcie to post locatino to server
+                                              printLog("triggerLocationPostTimer run");
+                                              startService(new Intent(getApplicationContext(), PostLocationService.class));
+                                            }
+
+                                          },
+            //Set how long before to start calling the TimerTask (in milliseconds)
+            0,
+            //Set the amount of time between each execution (in milliseconds)
+            60 * 1000);
+  }
+
+  // New Method for crteateForgroundNotification
+  private Notification crteateForgroundNotification(Context context,
+                                                    String message, String channelId) {
+
+    printLog("crteateForgroundNotification");
+
+    long when = System.currentTimeMillis();
+    String title = "VAS";
+
+    // Open Activity when notification is clicked
+    // set intent so it does not start a new activity
+    Intent notificationIntent = new Intent(context, MyTicketActivity.class);
+    notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP
+            | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+    PendingIntent pendingIntent = PendingIntent.getActivity(context, 0,
+            notificationIntent, 0);
+
+    //initialize Notification
+    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+      Notification.Builder builder = new Notification.Builder(context, channelId).
+              setSmallIcon(R.drawable.bg_service_icon)
+              .setContentTitle(title)
+              .setContentText(message)
+              .setContentIntent(pendingIntent);
+      return builder.build();
+    } else {
+
+      //initialize Notification
+      NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context)
+              .setSmallIcon(R.drawable.bg_service_icon)
+              .setContentTitle(title)
+              .setContentText(message)
+              .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+              .setContentIntent(pendingIntent);
+
+      return mBuilder.build();
     }
-
-    @Override
-    public void onConnected(Bundle arg0) {
-        // TODO Auto-generated method stub
-        //  Utils.printLog("my service GoogleApiclient onConnected");
-        printLog("GoogleApiclient onConnected call");
-        createLocationRequest();
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            return;
-        }
-        Location mLastLocation = LocationServices.FusedLocationApi
-                .getLastLocation(mGoogleApiClient);
-        UtilityFunction.saveLocationToPreference(getApplicationContext(), mLastLocation);
-        startLocationUpdates();
-        //Notification
-        startForeground(1119,
-                crteateForgroundNotification(getApplicationContext(), "Tracking Service"));
-    }
-
-    @Override
-    public void onConnectionSuspended(int arg0) {
-        // TODO Auto-generated method stub
-        printLog("GoogleApiclient onConnectionSuspended call");
-        mGoogleApiClient.connect();
-        //   Utils.printLog("my service GoogleApiclient onConnectionSuspended");
-
-    }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult arg0) {
-        printLog("onConnectionFailed call");
-        printLog(arg0.toString());
-        // TODO Auto-generated method stub
-        //  Utils.printLog("my service GoogleApiclient onConnectionFailed");
-    }
-
-    @Override
-    public void onLocationChanged(Location updatelocation) {
-        printLog("onLocationChanged call");
-
-        // TODO Auto-generated method stub
-        UtilityFunction.saveLocationToPreference(getApplicationContext(), updatelocation);
-
-        if (updatelocation != null)
-            printLog("location lat:" + updatelocation.getLatitude() + " , lng:" + updatelocation.getLongitude());
-    }
-
-    protected synchronized void buildGoogleApiClient() {
-        if (mGoogleApiClient != null && mGoogleApiClient.isConnected()) {
-            stopLocationUpdates();
-            mGoogleApiClient.disconnect();
-        }
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .addApi(LocationServices.API)
-                .build();
-        mGoogleApiClient.connect();
-        printLog("buildGoogleApiClient call");
-    }
-
-    protected void createLocationRequest() {
-        printLog("createLocationRequest call");
-        mLocationRequest = LocationRequest.create();
-        mLocationRequest.setInterval(UPDATE_LOCATION_INTERVAL);
-        mLocationRequest.setFastestInterval(UPDATE_LOCATION_INTERVAL);
-        //  mLocationRequest.setSmallestDisplacement(5);
-        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-    }
-
-    protected void startLocationUpdates() {
-        printLog("startLocationUpdates call");
-
-        if (!mRequestingLocationUpdates && mGoogleApiClient != null && mGoogleApiClient.isConnected()) {
-            mRequestingLocationUpdates = true;
-            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                return;
-            }
-            LocationServices.FusedLocationApi.requestLocationUpdates(
-                    mGoogleApiClient, mLocationRequest, this);
-        }
-    }
-
-    protected void stopLocationUpdates() {
-        printLog("stopLocationUpdates call");
-
-        if (mGoogleApiClient != null && mGoogleApiClient.isConnected()
-                && mRequestingLocationUpdates) {
-            mRequestingLocationUpdates = false;
-            LocationServices.FusedLocationApi.removeLocationUpdates(
-                    mGoogleApiClient, this);
-        }
-        if (locationPostTimer != null) {
-            locationPostTimer.cancel();
-            locationPostTimer = null;
-        }
-    }
-
-
-    @Override
-    public void onDestroy() {
-        // TODO Auto-generated method stub
-        instans = null;
-
-        stopLocationUpdates();
-        //  Utils.printLog("Service destroy");
-        super.onDestroy();
-    }
-
-    private void triggerLocationPostTimer() {
-        printLog("triggerLocationPostTimer");
-        if (locationPostTimer != null) {
-            locationPostTimer.cancel();
-
-        }
-        locationPostTimer = new Timer();
-        //Set the schedule function and rate
-        locationPostTimer.scheduleAtFixedRate(new TimerTask() {
-
-                                                  @Override
-                                                  public void run() {
-                                                      //Called each time when 1000 milliseconds (1 second) (the period parameter)
-                                                      //Trigger anther servcie to post locatino to server
-                                                      printLog("triggerLocationPostTimer run");
-                                                      startService(new Intent(getApplicationContext(), PostLocationService.class));
-                                                  }
-
-                                              },
-                //Set how long before to start calling the TimerTask (in milliseconds)
-                0,
-                //Set the amount of time between each execution (in milliseconds)
-                60 * 1000);
-    }
-
-    // New Method for crteateForgroundNotification
-    private Notification crteateForgroundNotification(Context context,
-                                                      String message) {
-
-        printLog("crteateForgroundNotification");
-
-        long when = System.currentTimeMillis();
-        String title = "VAS";
-
-        // Open Activity when notification is clicked
-        // set intent so it does not start a new activity
-        Intent notificationIntent = new Intent(context, MyTicketActivity.class);
-        notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP
-                | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0,
-                notificationIntent, 0);
-
-        //initialize Notification
-        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context)
-                .setSmallIcon(R.drawable.bg_service_icon)
-                .setContentTitle(title)
-                .setContentText(message)
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                .setContentIntent(pendingIntent);
-
-        return mBuilder.build();
-    }
+  }
 
 }
